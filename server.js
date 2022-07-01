94dadddafcc7e0d586a1d090f53d9d873a18b7c7
RegularKim <regular.jk.kim@gmail.com>
2022-07-02 00:32:01 +0900

    0702

--- server.js
+++ server.js
@@ -2,9 +2,21 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 app.use(express.urlencoded({ extended: true }));
+require("dotenv").config();
 
-app.listen(8080, function () {
-    console.log("Hi! This is your node.js server response!");
+var db;
+const MongoClient = require("mongodb").MongoClient;
+app.set("view engine", "ejs");
+
+MongoClient.connect(process.env.DB_URL, function (err, client) {
+    app.listen(process.env.PORT, function () {
+        if (err) {
+            return console.log(err);
+        }
+        db = client.db("todoapp");
+
+        console.log("Hi! This is your node.js server response and MongoDB!");
+    });
 });
 
 app.get("/pet", function (req, res) {
@@ -22,6 +34,28 @@ app.get("/write", function (req, res) {
 });
 
 app.post("/add", function (req, res) {
+    db.collection("post").insertOne(
+        { title: req.body.title, date: req.body.date },
+        function (err, res) {
+            console.log("save complete!");
+        }
+    );
+
     res.send("send complete!");
-    console.log(req.body);
+
+    console.log("title : " + req.body.title);
+    console.log("date : " + req.body.date);
+});
+
+app.get("/list", function (req, res) {
+    // post라는 컬렉션안에 있는 모든 데이터를 꺼내주세요.
+
+    db.collection("post")
+        .find()
+        .toArray(function (err, result) {
+            console.log(result);
+            res.render("list.ejs", { posts: result });
+        });
+
+    console.log("complete");
 });
