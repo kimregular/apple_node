62ceba14c790c0a8dc1b562b5cc13888fe62a78b
RegularKim <regular.jk.kim@gmail.com>
2022-07-03 06:10:12 +0900

    0703

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+
 app.use(express.urlencoded({ extended: true }));
 require("dotenv").config();
+
 const methodOverride = require("method-override");
 app.use(methodOverride("_method"));
+
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const session = require("express-session");
+
+app.use(
+    session({ secret: "비밀코드", resave: true, saveUninitialized: false })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 var db;
 const MongoClient = require("mongodb").MongoClient;
@@ -123,4 +135,71 @@ app.put("/edit", function (req, res) {
             res.redirect("/list");
         }
     );
+});
+
+app.get("/login", function (req, res) {
+    res.render("login.ejs");
+});
+
+app.post(
+    "/login",
+    passport.authenticate("local", {
+        failureRedirect: "/fail",
+    }),
+    function (req, res) {
+        res.redirect("/");
+    }
+);
+
+app.get("/mypage", isLogin, function (req, res) {
+    console.log(req.user);
+    res.render("mypage.ejs", { 사용자: req.user });
+});
+
+function isLogin(req, res, next) {
+    if (req.user) {
+        next();
+    } else {
+        res.send("you didn't be loged in");
+    }
+}
+
+passport.use(
+    new LocalStrategy(
+        {
+            usernameField: "id",
+            passwordField: "pw",
+            session: true,
+            passReqToCallback: false,
+        },
+        function (입력한아이디, 입력한비번, done) {
+            //console.log(입력한아이디, 입력한비번);
+            db.collection("login").findOne(
+                { id: 입력한아이디 },
+                function (에러, 결과) {
+                    if (에러) return done(에러);
+
+                    if (!결과)
+                        return done(null, false, {
+                            message: "존재하지않는 아이디요",
+                        });
+                    if (입력한비번 == 결과.pw) {
+                        return done(null, 결과);
+                    } else {
+                        return done(null, false, { message: "비번틀렸어요" });
+                    }
+                }
+            );
+        }
+    )
+);
+
+passport.serializeUser(function (user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function (아이디, done) {
+    db.collection("login").findOne({ id: 아이디 }, function (err, result) {
+        done(null, result);
+    });
 });
